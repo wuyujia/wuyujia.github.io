e142b6c6e7cae3c9dd4904b4dc87e15a82d570a8
wuyujia <wyj_hbnu10@126.com>
2023-06-01 07:23:05 +0000

    Deploying to master from @ wuyujia/wuyujia.github.io@c6d1e48184b85f26f7c12f4af9a9b52175603ff3 🚀

---
+++ assets/index.html-b95740a4.js
@@ -0,0 +1,10 @@
+import{_ as n,W as s,X as a,a2 as e}from"./framework-aa83d01c.js";const i={},l=e(`<h2 id="查询本机外网ip" tabindex="-1"><a class="header-anchor" href="#查询本机外网ip" aria-hidden="true">#</a> 查询本机外网IP</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">curl</span> cip.cc
+
+IP      <span class="token builtin class-name">:</span> xxx.xx.xx.xx
+地址    <span class="token builtin class-name">:</span> 中国  北京
+运营商  <span class="token builtin class-name">:</span> 电信
+
+数据二  <span class="token builtin class-name">:</span> 北京市 <span class="token operator">|</span> 电信
+
+数据三  <span class="token builtin class-name">:</span> 中国北京北京市 <span class="token operator">|</span> 电信
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[l];function d(t,r){return s(),a("div",null,c)}const p=n(i,[["render",d],["__file","index.html.vue"]]);export{p as default};
