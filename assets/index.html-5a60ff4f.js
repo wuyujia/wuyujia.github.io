3f6645f7bf7c1d74e055abb31895fc46edd189c4
wuyujia <wyj_hbnu10@126.com>
2023-06-30 08:50:42 +0000

    Deploying to master from @ wuyujia/wuyujia.github.io@9d144be7f126a68c43abd7ef34b4e144e44878d4 🚀

---
+++ assets/index.html-5a60ff4f.js
@@ -0,0 +1,10 @@
+import{_ as e,W as a,X as s,Y as i,$ as l,a1 as c,a2 as t,C as d,Z as r}from"./framework-aa83d01c.js";const o={},u=t(`<h2 id="查询本机外网ip" tabindex="-1"><a class="header-anchor" href="#查询本机外网ip" aria-hidden="true">#</a> 查询本机外网IP</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">curl</span> cip.cc
+
+IP      <span class="token builtin class-name">:</span> xxx.xx.xx.xx
+地址    <span class="token builtin class-name">:</span> 中国  北京
+运营商  <span class="token builtin class-name">:</span> 电信
+
+数据二  <span class="token builtin class-name">:</span> 北京市 <span class="token operator">|</span> 电信
+
+数据三  <span class="token builtin class-name">:</span> 中国北京北京市 <span class="token operator">|</span> 电信
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="日志切割" tabindex="-1"><a class="header-anchor" href="#日志切割" aria-hidden="true">#</a> 日志切割</h2>`,3);function p(m,v){const n=d("RouterLink");return a(),s("div",null,[u,i("p",null,[l(n,{to:"/linux/crontab.html"},{default:c(()=>[r("日志切割")]),_:1})])])}const b=e(o,[["render",p],["__file","index.html.vue"]]);export{b as default};
